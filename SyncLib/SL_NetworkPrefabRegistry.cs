dfa209ebcf208f4960e577d2ce1ec26c7bd90b9e
MrBanana <logande4610@gmail.com>
2026-07-11 03:23:57 -0500

    Add project files.

---
+++ SyncLib/SL_NetworkPrefabRegistry.cs
@@ -0,0 +1,276 @@
+﻿using Alta.Networking;
+using Alta.Networking.Internal;
+using HarmonyLib;
+using MelonLoader;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace SyncLib {
+    /// <summary>
+    /// A class designed to assist in easily registering prefabs as NetworkPrefabs in Alta's systems for later use.
+    /// </summary>
+    public class SL_NetworkPrefabRegistry : MonoBehaviour {
+        public static List<NetworkPrefab> RegisteredCustomPrefabs = new List<NetworkPrefab>();
+
+        internal static Dictionary<string, Dictionary<string, int>> HashIds { get; set; }
+        internal static string jsonData = null;
+
+        public static EntityManager entityManager = null;
+
+        /// <summary>
+        /// Called when prefabs are avaliable to be registered by SyncLib.
+        /// </summary>
+        public static Action RegisterPrefabs;
+
+        internal static bool recievedSyncData = false;
+
+        private static Dictionary<string, int> GetOrCreateNewModHashIds(string modId) {
+            if (!HashIds.TryGetValue(modId, out Dictionary<string, int> prefabIds)) {
+                prefabIds = new Dictionary<string, int>();
+                HashIds.Add(modId, prefabIds);
+            }
+
+            return prefabIds;
+        }
+
+        private static void ReadJsonFile() {
+            if (NetworkSceneManager.IsServer && jsonData is null) {
+                string modPrefabFilePath = Path.GetFullPath(Path.Combine(Application.dataPath, "..", "modHashIDs.json"));
+
+                if (!File.Exists(modPrefabFilePath)) {
+                    HashIds = new Dictionary<string, Dictionary<string, int>>();
+
+                    return;
+                }
+
+                jsonData = File.ReadAllText(modPrefabFilePath);
+            }
+
+            try {
+                Dictionary<string, Dictionary<string, int>>? jsonArray = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, int>>>(jsonData);
+
+                if (jsonArray is null) {
+                    MelonLogger.Error("Failed to read Json modHashIDs file.");
+
+                    // Quitting just so any save data on the server won't get lost.
+                    if (NetworkSceneManager.IsServer)
+                        Application.Quit();
+
+                    return;
+                }
+
+                HashIds = jsonArray;
+            }
+            catch (Exception ex) {
+                MelonLogger.Error("Failed to read Json modHashIDs file.");
+
+                // Quitting just so any save data on the server won't get lost.
+                if (NetworkSceneManager.IsServer)
+                    Application.Quit();
+
+                return;
+            }
+        }
+
+        internal static bool hasRegistered = false;
+
+        private static HashSet<int> takenHashIds;
+
+        internal static void RegisterIntoGame() {
+            if (hasRegistered)
+                return;
+
+            takenHashIds = new HashSet<int>(SyncLib.ExistingHashIDs);
+
+            MelonLogger.Msg("Registering custom prefabs...");
+
+            ReadJsonFile();
+
+            entityManager = Traverse.Create(NetworkSceneManager.Current).Field("entityManager").GetValue<EntityManager>();
+
+            RegisterPrefabs?.Invoke();
+
+            if (NetworkSceneManager.IsServer) {
+                string modPrefabFilePath = Path.GetFullPath(Path.Combine(Application.dataPath, "..", "modHashIDs.json"));
+
+                string jsonArray = JsonConvert.SerializeObject(HashIds, Formatting.Indented);
+
+                File.WriteAllText(modPrefabFilePath, jsonArray);
+            }
+
+            hasRegistered = true;
+        }
+
+        internal static int getNextAvaliableHashId() {
+            if (takenHashIds is null)
+                return -1;
+
+            int nextAvaliable = 1;
+            while (takenHashIds.Contains(nextAvaliable)) {
+                nextAvaliable++;
+            }
+
+            return nextAvaliable;
+        }
+
+        /// <summary>
+        /// Registers a prefab as a NetworkPrefab in Alta's sytems for later use.
+        /// <br></br>
+        /// <br></br>
+        /// The SyncLibPrefabId only matters to SyncLib, please do not change it after publishing your mod.
+        /// <br></br>
+        /// Your mod is automatically assigned a Hash upon first registry per server.
+        /// </summary>
+        /// <param name="prefab"></param>
+        /// <returns></returns>
+        public static NetworkPrefab? RegisterPrefab(MelonMod mod, GameObject prefab, string SyncLibPrefabId) {
+            if (HashIds is null) {
+                MelonLogger.Error("SyncLib attempted to register a custom prefab. But the HashIds reference was null.");
+
+                if (NetworkSceneManager.IsServer) {
+                    Application.Quit();
+                }
+
+                return null;
+            }
+
+            if (takenHashIds is null) {
+                MelonLogger.Error("SyncLib attempted to register a custom prefab. But the takenHashIds reference was null.");
+
+                if (NetworkSceneManager.IsServer) {
+                    Application.Quit();
+                }
+
+                return null;
+            }
+
+            if (entityManager is null) {
+                MelonLogger.Error("SyncLib attempted to register a custom prefab. But the entityManager reference was null.");
+
+                if (NetworkSceneManager.IsServer) {
+                    Application.Quit();
+                }
+
+                return null;
+            }
+
+            if (mod is null) {
+                MelonLogger.Error("SyncLib attempted to register a custom prefab. However the 'mod' parameter was null.");
+
+                if (NetworkSceneManager.IsServer) {
+                    Application.Quit();
+                }
+
+                return null;
+            }
+
+            if (string.IsNullOrWhiteSpace(SyncLibPrefabId) || SyncLibPrefabId.Length < 3) {
+                MelonLogger.Error("SyncLib attempted to register a custom prefab with an invalid SyncLibPrefabId. Please make sure your SyncLibPrefabId has at least three characters.");
+
+                if (NetworkSceneManager.IsServer) {
+                    Application.Quit();
+                }
+
+                return null;
+            }
+
+            if (prefab is null) {
+                MelonLogger.Error("SyncLib attempted to register a custom prefab that was null.");
+
+                if (NetworkSceneManager.IsServer) {
+                    Application.Quit();
+                }
+
+                return null;
+            }
+
+            NetworkPrefab networkprefab = prefab.GetComponent<NetworkPrefab>();
+
+            if (networkprefab is null)
+                networkprefab = prefab.AddComponent<NetworkPrefab>();
+            else {
+                MelonLogger.Error("You cannot register a prefab that already has a NetworkPrefab.");
+                    return null;
+            }
+
+            NetworkEntity entity = prefab.GetComponent<NetworkEntity>();
+            if (entity is null)
+                entity = prefab.AddComponent<NetworkEntity>();
+            else {
+                MelonLogger.Error("You cannot register a prefab that already has a NetworkEntity.");
+                    return null;
+            }
+
+            FieldInfo fieldInfoNP = typeof(NetworkPrefab).GetField("entity", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (fieldInfoNP == null)
+                throw new Exception("Couldn't find NetworkPrefab.entity");
+
+            fieldInfoNP.SetValue(networkprefab, entity);
+
+            FieldInfo fieldInfoNE = typeof(NetworkEntity).GetField("prefab", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (fieldInfoNE == null)
+                throw new Exception("Couldn't find NetworkEntity.prefab");
+
+            fieldInfoNE.SetValue(entity, networkprefab);
+
+            networkprefab.Initialize();
+
+            Dictionary<string, int>? hashIds = GetOrCreateNewModHashIds(mod.ID);
+
+            if (hashIds is null) {
+                MelonLogger.Error("SyncLib attempted to register a custom prefab, but something unexpected happened.");
+                    return null;
+            }
+
+            void SetHashId(int HashId) {
+                FieldInfo fieldInfoHash = typeof(NetworkPrefab).GetField("hash", BindingFlags.NonPublic | BindingFlags.Instance);
+                fieldInfoHash.SetValue(networkprefab, HashId);
+            }
+
+            bool alreadyContainedID = false;
+
+            foreach (var item in hashIds) {
+                string syncLibPrefabId = item.Key;
+                int hashId = item.Value;
+
+                if (syncLibPrefabId == SyncLibPrefabId) {
+                    if (hashId > 0) {
+                        SetHashId(hashId);
+
+                        alreadyContainedID = true;
+
+                        break;
+                    }
+                }
+            }
+
+            if (!alreadyContainedID) {
+                int nextAvaliable = getNextAvaliableHashId();
+
+                if (nextAvaliable <= 0) {
+                    MelonLogger.Error("SyncLib attempted to register a custom prefab, but getNextAvaliableHashId returned an error.");
+                        return null;
+                }
+
+                takenHashIds.Add(nextAvaliable);
+
+                hashIds.Add(SyncLibPrefabId, nextAvaliable);
+
+                SetHashId(nextAvaliable);
+            }
+
+            NetworkPrefab[] prefabArray = new NetworkPrefab[] { networkprefab };
+
+            MethodInfo methodInfo = typeof(PrefabManager).GetMethod("AddToPrefabMap", BindingFlags.NonPublic | BindingFlags.Static);
+            methodInfo.Invoke(null, new object[] { prefabArray });
+
+            return networkprefab;
+        }
+    }
+}
---
+++ SyncLib/SyncLib.cs
@@ -0,0 +1,133 @@
+﻿using Alta.Networking;
+using Alta.Networking.Internal;
+using Alta.NetworkingTransport;
+using Alta.Utilities;
+using HarmonyLib;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Assembly = System.Reflection.Assembly;
+
+[assembly: MelonInfo(typeof(SyncLib.SyncLib), "SyncLib", "1.0.0", "MrDuckTheFifth")]
+[assembly: MelonGame("Alta", "A Township Tale")]
+namespace SyncLib {
+    public class SyncLib : MelonMod {
+        internal const MessageType SyncLibJson = (MessageType)500;
+
+        internal static GameObject registryObject;
+
+        private static int[] _existingHashIDs;
+
+        public static int[] ExistingHashIDs => _existingHashIDs;
+
+        public override void OnInitializeMelon() {
+            base.OnInitializeMelon();
+
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SyncLib.ExistingPrefabIDs.txt")) {
+                using (StreamReader reader = new StreamReader(stream)) {
+                    string text = reader.ReadToEnd();
+
+                    string[] list = text
+                        .Replace("\r", "")
+                        .Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    _existingHashIDs = new int[list.Length];
+
+                    for (int i = 0; i < list.Length; i++) {
+                        if (int.TryParse(list[i], out int value)) {
+                            _existingHashIDs[i] = value;
+                        }
+                        else {
+                            LoggerInstance.Error(
+                                $"Failed to parse prefab Id: '{list[i]}'"
+                            );
+                        }
+                    }
+                }
+            }
+        }
+
+        public override void OnLateInitializeMelon() {
+            base.OnLateInitializeMelon();
+
+            if (_existingHashIDs is null) {
+                LoggerInstance.Error("Something went wrong while reading ExistingPrefabIDs.");
+
+                return;
+            }
+
+            string modPrefabFilePath = Path.GetFullPath(Path.Combine(Application.dataPath, "..", "modHashIDs.json"));
+
+            registryObject = new GameObject("SyncLib - NetworkPrefabRegistry");
+            registryObject.AddComponent<SL_NetworkPrefabRegistry>();
+
+            if (File.Exists(modPrefabFilePath)) {
+                string jsonData = File.ReadAllText(modPrefabFilePath);
+
+                SL_NetworkPrefabRegistry.jsonData = jsonData;
+            }
+
+            DontDestroyOnLoad.DontDestroyOnLoad(registryObject);
+        }
+    }
+
+    [HarmonyPatch(typeof(Player), "InitializePlayerOnServer", new Type[] { typeof(Connection), typeof(PlayerMode), typeof(PlatformTarget), typeof(IAltaFile) })]
+    internal static class PlayerPatch {
+        private static void Prefix(Connection connection) {
+            if (NetworkSceneManager.IsServer) {
+                connection.Send(null, SyncLib.SyncLibJson, Serialize);
+            }
+        }
+
+        public static void Serialize(Connection connection, Alta.Serialization.Stream stream) {
+            string jsonData = SL_NetworkPrefabRegistry.jsonData;
+            string finalizedJsonData = "SYNCLIB-JSONSYNC:";
+            finalizedJsonData += jsonData;
+            stream.SerializeString(ref finalizedJsonData);
+        }
+    }
+
+    public class JsonConnectionReciever : ConnectionReceiver {
+        public JsonConnectionReciever(Connection connection, ConnectionChannel channel) : base(connection, channel) { }
+
+        public override void ProcessExistingQueues() { }
+
+        public override void ReceivePacket(ITransportSocket socket, ArraySegment<byte> data) {
+            if (SL_NetworkPrefabRegistry.hasRegistered)
+                return;
+
+            while (data.Count > 0) {
+                MessageType messageType;
+                int num = MessageProcessor.ProcessSingleMessageFromData(connection, data, channel, out messageType);
+                int offset = data.Offset + num;
+                int count = data.Count - num;
+                data = new ArraySegment<byte>(data.Array, offset, count);
+
+                if (messageType == SyncLib.SyncLibJson) {
+                    using MemoryStream stream = new MemoryStream(data.Array, data.Offset, data.Count);
+
+                    using (StreamReader reader = new StreamReader(stream)) {
+                        string text = reader.ReadToEnd();
+
+                        if (!text.StartsWith("SYNCLIB-JSONSYNC:"))
+                            continue;
+
+                        text = text.Replace("SYNCLIB-JSONSYNC:", "");
+
+                        SL_NetworkPrefabRegistry.jsonData = text;
+                    }
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(PrefabManager), "PrepareSpawnSetups")]
+    internal static class PrefabManagerPatch {
+        private static void Postfix() {
+            if(NetworkSceneManager.IsServer || SL_NetworkPrefabRegistry.recievedSyncData)
+                SL_NetworkPrefabRegistry.RegisterIntoGame();
+        }
+    }
+}
